1b4b09bde2eedf8777c80591a2b6215b6bd4dc09
Shokhrukh Rustamaliev <black.doppi@gmail.com>
2022-06-11 23:17:33 +0500

    compare

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 'use strict';
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-const app         = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const app = express();
 fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
@@ -11,6 +11,16 @@ const someOtherPlaintextPassword = 'pass123';
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 const bcrypt = require('bcrypt')
+
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    /* Store hash in your db */
+    console.log(hash)
+
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+        /* res == true or false */
+        console.log(res)
+    })
+})
 
 
 //END_ASYNC
@@ -50,4 +60,4 @@ const bcrypt = require('bcrypt')
 
 
 
-app.listen(process.env.PORT || 3000, () => {});
+app.listen(process.env.PORT || 3000, () => { });
