c3ced4d12954d17796841331395e67431aa84fc2
Shokhrukh Rustamaliev <black.doppi@gmail.com>
2022-06-11 23:35:42 +0500

    sync

--- server.js
+++ server.js
@@ -20,9 +20,10 @@ bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
 //END_ASYNC
 
 //START_SYNC
-
-
-
+const hash = bcrypt.hashSync(myPlaintextPassword, saltRounds)
+console.log(hash)
+const result = bcrypt.compareSync(myPlaintextPassword, hash)
+console.log(result)
 //END_SYNC
 
 
